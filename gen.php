bfab200c052d96ce71067b38ee6d39dbd67b5e0f
zinntikumugai <uesitananame55@gmail.com>
2016-04-11 22:52:12 +0900

    Update gen.php
    コメント追加

--- gen.php
+++ gen.php
@@ -2,6 +2,12 @@
 function gen_char( $str, $encord, $str_len, $gn ){
   /* 乱数生成 */
   /* 参考 http://webkaru.net/php/function-substr/ */
+  /* 
+  $gn 生成文字数
+  $encord 文字エンコード
+  $str 生成元の文字
+  $str_len $strの文字数
+  */
 	for( ; $i < $gn; $i++) {
 		$strs .= mb_substr( $str, mt_rand(0, $str_len), 1 ,$encord );
 	}
