eb610d98ff1538774c0f6f8694124b3bac03e445
zinntikumugai <uesitananame55@gmail.com>
2016-04-11 23:20:40 +0900

    Update gen.php
    生成元の文字数を内部で求めるように変更

--- gen.php
+++ gen.php
@@ -1,5 +1,5 @@
 <?php
-function gen_char( $str, $encord, $str_len, $gn ){
+function gen_char( $str, $encord, $gn ){
   /* 乱数生成 */
   /* 参考 http://webkaru.net/php/function-substr/ */
   /* 
@@ -8,6 +8,8 @@ function gen_char( $str, $encord, $str_len, $gn ){
   $str 生成元の文字
   $str_len $strの文字数
   */
+  	$str_len = mb_strlen( $str, $encord)-1;
+  	
 	for( ; $i < $gn; $i++) {
 		$strs .= mb_substr( $str, mt_rand(0, $str_len), 1 ,$encord );
 	}
